b8c69d5c05e9abbbe76246778b3d2097448e54cd
Macbook <macbook@MacBook-Pro.local>
2025-08-07 22:00:10 +0500

    Initial header

--- js/header.js
+++ js/header.js
@@ -3,10 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const navMenu = document.querySelector(".nav-menu");
     const dropdowns = document.querySelectorAll(".has-dropdown");
 
+    // Hamburger toggle
     hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
     });
+
+    // Close menu on normal link click
     document.querySelectorAll(".nav-link").forEach((link) => {
         if (!link.parentElement.classList.contains("has-dropdown")) {
             link.addEventListener("click", () => {
@@ -17,11 +20,17 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     });
+
+    // Dropdown click
     dropdowns.forEach((dropdown) => {
-        dropdown.addEventListener("click", (event) => {
+        dropdown.addEventListener("click", function (event) {
             if (window.innerWidth <= 992) {
-                event.preventDefault();
-                dropdown.classList.toggle("active");
+                // Toggle dropdown menu without preventing link clicks
+                const isLink = event.target.tagName.toLowerCase() === 'a';
+                if (!isLink) {
+                    event.preventDefault();
+                    dropdown.classList.toggle("active");
+                }
             }
         });
     });
